c768df82bd3b5fd505fe38f82c00f1224a139d06
Dmitriy Trochshenko <dmitriy.trochshenko@gmail.com>
2015-09-09 18:50:27 +0600

    fixed base dirs

--- fb_app/settings/base.py
+++ fb_app/settings/base.py
@@ -1,7 +1,7 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 base = lambda directory: os.path.join(BASE_DIR, directory)
 project = lambda directory: os.path.join(PROJECT_DIR, directory)
